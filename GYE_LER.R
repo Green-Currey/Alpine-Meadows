11b702eadf98c37a7b7b9a385ee08e269ad5c8f2
Bryce Currey <57914237+Green-Currey@users.noreply.github.com>
2021-02-04 14:16:22 -0700

    Add files via upload

---
+++ GYE_LER.R
@@ -0,0 +1,46 @@
+# LER analysis ------------------------------------------------------------
+
+library(raster)
+library(dplyr)
+dem <- raster('d:/gis_data/GYE/Raster/30m_dem_GYE.tif')
+dem <- aggregate(dem, fact = 33.333, fun = mean)
+dem
+# plot(dem)
+# global.mat2 <- global.mat[2000:2300, 25000:25300]
+dem.mat <- dem %>% as.matrix()
+
+# LER is a 10 sq km window that checks for a relief of 300m
+# around each cell. If 300m of relief is found within the LER window, the cell
+# is deemed a mountain.
+
+for (i in seq(nrow(dem.mat))) {
+     for (j in seq(ncol(dem.mat))) {
+          
+          if (is.na(dem.mat[i,j])) {global.mat2[i,j] <- NA
+          
+          } else {
+               
+               # this section of logical statements checks for the edges
+               # of the map and adjusts the LER window
+               if (i < 5) {i.seq <- i:(i+5)
+               } else if (i > nrow(dem.mat)-5) {i.seq <- (i-5):nrow(dem.mat)
+               } else {i.seq <- (i-5):(i+5)
+               }
+               
+               
+               if (j < 5) {j.seq <- j:(j+5)
+               } else if (j > ncol(dem.mat)-5) {(j-5):ncol(dem.mat)
+               } else {j.seq <- (j-5):(j+5)
+               }
+               
+               LER <- dem.mat[i.seq, j.seq]
+               
+               if (sum(is.na(LER)) == (nrow(LER)*ncol(LER))) {LER.gye[i,j] <- NA} else {relief <- dem.mat[i,j] - min(LER, na.rm = T)}
+               
+               
+               if (relief >= 300) {LER.gye[i,j] <- 1} else {LER.gye[i,j] <- NA}
+          }
+     }
+}
+
+write.csv(LER.gye, 'd:/gis_data/GYE/LER_GYE.csv')
