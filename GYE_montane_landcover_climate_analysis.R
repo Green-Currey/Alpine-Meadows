26182ce6a4306d7cf3c9e21f1f2c22dd0d3b4b3e
Bryce Currey <57914237+Green-Currey@users.noreply.github.com>
2021-02-10 08:08:03 -0700

    Add files via upload

---
+++ GYE_montane_landcover_climate_analysis.R
@@ -0,0 +1,43 @@
+# Alpine Montane Landcover and Precipitation analysis
+
+rm(list = ls())
+cat('\014')
+
+library(raster)
+library(rgdal)
+library(dplyr)
+
+
+# Landcover ---------------------------------------------------------------
+# Grass/shrubland = 1
+# Forest = 2
+# Other = 3
+
+montane <- raster('z:/gis_data/gye/Raster/30m_montane_GYE_masked.tif')
+montane
+plot(montane)
+
+lc <- raster('z:/gis_data/gye/Raster/GYE_grass_forest_LC_wgs.tif') %>%
+     resample(montane, method = 'ngb')
+lc
+plot(lc)
+
+
+montane.lc <- montane*lc
+montane.lc
+plot(montane.lc)
+writeRaster(montane.lc, 'z:/gis_data/gye/Raster/GYE_grass_forest_LC_wgs.tif', overwrite = T)
+
+
+# Precipitation -----------------------------------------------------------
+
+gye <- readOGR('z:/gis_data/gye/Shapefile/GYE_WGS_OUTLINE.shp')
+map <- raster('z:/gis_data/climate/wc2.1_30s_bio/wc2.1_30s_bio_12.tif')
+map
+map.masked <- mask(map, gye)
+map.masked <- crop(map.masked, gye)
+plot(map.masked)
+
+map.gye <- map.masked %>% 
+     resample(montane.lc)
+writeRaster(map.gye, 'z:/gis_data/gye/Raster/GYE_MAP_30m.tif', overwrite = T)
