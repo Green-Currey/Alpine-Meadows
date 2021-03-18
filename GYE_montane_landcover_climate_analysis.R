3a446a69f6aa534257e02bb44f98718e8e79b3d1
Bryce Currey <brycecurrey93@gmail.com>
2021-03-18 14:02:42 -0600

    Update GYE_montane_landcover_climate_analysis.R

--- GYE_montane_landcover_climate_analysis.R
+++ GYE_montane_landcover_climate_analysis.R
@@ -9,16 +9,27 @@ library(dplyr)
 
 
 # Landcover ---------------------------------------------------------------
+# For GYE_grass_forest_lc.tif
 # Grass/shrubland = 1
 # Forest = 2
 # Other = 3
+
+# OR
+
+# For GYE_grass_shrub_forest_lc.tif
+# Grassland = 1
+# Shrubland = 2
+# Forest = 3
+# Other = 4
 
 montane <- raster('z:/gis_data/gye/Raster/30m_montane_GYE_masked.tif')
 montane
 plot(montane)
 
-lc <- raster('z:/gis_data/gye/Raster/GYE_grass_forest_LC_wgs.tif') %>%
-     resample(montane, method = 'ngb')
+# have to project
+lc <- raster('z:/gis_data/gye/Raster/GYE_grass_shrub_forest_LC.tif') %>%
+     # project()
+     # resample(montane, method = 'ngb')
 lc
 plot(lc)
 
@@ -41,3 +52,11 @@ plot(map.masked)
 map.gye <- map.masked %>% 
      resample(montane.lc)
 writeRaster(map.gye, 'z:/gis_data/gye/Raster/GYE_MAP_30m.tif', overwrite = T)
+
+
+map.gye <- raster('z:/gis_data/gye/Raster/GYE_MAP_30m.tif')
+map.500 <- map.gye>=500
+map.600 <- map.gye>=600
+
+writeRaster(map.500, 'z:/gis_data/gye/Raster/GYE_MAP_500mm.tif', overwrite = T)
+writeRaster(map.600, 'z:/gis_data/gye/Raster/GYE_MAP_600mm.tif', overwrite = T)
