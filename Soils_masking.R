079ef505c1e10b3e8042aa2b2b6073db4d10d845
Bryce Currey <brycecurrey93@gmail.com>
2021-03-28 19:57:32 -0600

    Update Soils_masking.R

--- Soils_masking.R
+++ Soils_masking.R
@@ -1,19 +1,20 @@
+rm(list = ls())
+cat('\014')
+
+
 library(rgdal)
 library(raster)
 
 
-
-
-
-
 gye <- readOGR('z:/GIS_data/GYE/GYI_Huc10_OUTLINE/GYE_Huc10_OUTLINE.shp')
-taxorder <- raster('z:/gis_data/gye/raster/Soils/gye_taxorder.tif')
-taxsuborder <- raster('z:/gis_data/gye/raster/Soils/gye_taxsuborder.tif')
+# taxorder <- raster('z:/gis_data/gye/raster/Soils/gye_taxorder.tif')
+taxsuborder <- raster('z:/gis_data/gye/soils/GYE_cryoborolls2.tif'); taxsuborder
+# taxsuborder2  <- raster('z:/gis_data/gye/soils/GYE_cryobs.tif')
 
-
-gye2 <- spTransform(gye, CRSobj = crs(taxorder))
-taxorder2 <- mask(taxorder, gye2)
-taxorder3 <- crop(taxorder2, taxsuborder)
+# For tax order data
+# gye2 <- spTransform(gye, CRSobj = crs(taxorder))
+# taxorder2 <- mask(taxorder, gye2)
+# taxorder3 <- crop(taxorder2, taxsuborder)
 
 # taxorder3 == 1 for mollisols
 # taxsuborder == 1 for cryolls
@@ -21,22 +22,30 @@ taxorder3 <- crop(taxorder2, taxsuborder)
 
 
 
-rcl <- matrix(c(0, 1, 1, 1, 10, NA), ncol = 3, byrow = T)
+# rcl <- matrix(c(0, 1, 1, 1, 10, NA), ncol = 3, byrow = T)
+# rcl
+# 
+# taxorder.rcl <- reclassify(taxorder3, rcl)
+# plot(taxorder.rcl)
+
+rcl <- matrix(c(0,1,1,
+                1,4,1), ncol = 3, byrow = T)
 rcl
 
-taxorder.rcl <- reclassify(taxorder3, rcl)
-plot(taxorder.rcl)
 
-rcl <- matrix(c(-Inf,0.5, NA,
-                0.5, 1, 1, 
-                1, 11, NA, 
-                11, 12, 1, 
-                12, 35, NA), ncol = 3, byrow = T)
-rcl
+# rcl2 <- matrix(c(0,1,1,
+#                 1,9,0), ncol = 3, byrow = T)
+# rcl2
 
 taxsuborder.rcl <- reclassify(taxsuborder, rcl)
-plot(taxsuborder.rcl)
-writeRaster(taxsuborder.rcl, 'z:/GIS_data/GYE/Raster/taxsuborder_mask.tif')
-
+# taxsuborder.rcl2 <- reclassify(taxsuborder2, rcl2)
+taxsuborder.rcl
+plot(taxsuborder.rcl2)
+ 
+# taxsuborder.rcl.wgs <- projectRaster(taxsuborder.rcl,
+#                                      crs = '+proj=longlat +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +no_defs',
+#                                      method = 'ngb')
+writeRaster(taxsuborder.rcl, 'z:/GIS_data/GYE/Raster/taxsuborder_mask.tif', overwrite = T)
+writeRaster(taxsuborder.rcl2, 'z:/GIS_data/GYE/Raster/taxsuborder_mask2.tif', overwrite = T)
 
 
